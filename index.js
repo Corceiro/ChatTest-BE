ed30a202cc82446ac01d9652208a4076380af515
Corceiro <corceiro_junior@hotmail.com>
2024-06-09 18:29:37 +0100

    CRUD requests

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const port = process.env.PORT;
 const server = express();
 server.use(express.json());
 
+//Functions
+// --------------------------------------------------------------
+
+//Get all messages
+server.get('/messages', function(req,res) {
+
+}) 
+
 server.listen(port, () => {
     console.log("Express server listening on port ", port);
 });
