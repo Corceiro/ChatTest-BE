94c521f03d6ce9e973b07bf6f1be278d1adf3057
Corceiro <corceiro_junior@hotmail.com>
2024-06-11 17:16:16 +0100

    Add message

--- database.js
+++ database.js
@@ -18,11 +18,111 @@ const connPool = mysql.createPool({
 //For HealthCheck
 export async function ConnectDB() { }
 
+//Validate user
+export async function validateUser(user) {
+    var sql = 'select 1 from users where user_name = ?;';
+    var inserts = [user];
+    sql = mysql.format(sql, inserts);
+
+    console.log(sql);
+
+    return new Promise((resolve, reject) =>
+        connPool.query(sql, function(err, results, fields) {
+            if(err) throw err;
+            console.log("Result - ", results);
+            resolve(results);
+        })
+    );
+}
+
+//Validate user
+export async function validateUsers(user) {
+    let sql;
+    console.log(user.length);
+    if(user.length>1) {
+        sql = 'select 1 from users where user_name = ? or user_name = ?;';
+        var inserts = [user[0], user[1]];
+        sql = mysql.format(sql, inserts);
+        console.log("SQL -> " + sql);
+
+        return new Promise((resolve, reject) =>
+            connPool.query(sql, function(err, results, fields) {
+                if(err) throw err;
+                console.log("Results length: " + results.length);
+                if(results.length == 2) {
+                    resolve("Success");
+                } else if(results.length > 2) {
+                    resolve("User does not exist");
+                }
+            })
+        );
+    } else if (user.length == 1) {
+        sql = 'select 1 from users where user_name = ?;';
+        var inserts = [user[0]];
+        sql = mysql.format(sql, inserts);
+
+        console.log("SQL single -> " + sql);
+
+        return new Promise((resolve, reject) =>
+            connPool.query(sql, function(err, results, fields) {
+                if(err) throw err;
+                console.log("Result - ", results);
+                
+                resolve(results);
+            })
+        );
+    } else { return "More than 2 users provided"}
+}
+
 //Get all chats
 export async function getChat() { }
 //Get chat for user
-export async function getChatForUser() { }
+export async function getChatForUser(user) { 
+    var sql = 'select * from ;';
+    var inserts = [bookName, bookEditor, bookAuthor, bookName];
+    sql = mysql.format(sql, inserts);
+
+}
 //Add message
-export async function addMessages() { }
+export async function addMessage(user1, user2, message, timestamp) { 
+
+    //Validate if chat exists
+    var sql = 'select chat_id from chat where user1_id = ? and user2_id = ?;'
+    var inserts = [user1, user2];
+    sql = mysql.format(sql, inserts);
+
+    connPool.query(sql, function(err, results, fields) {
+        console.log("Results - " + results);
+        if(err) throw err;
+
+        //Chat does not exist
+        else if(!results) {
+            //Create chat
+            var sql = 'insert into chat (user1_id, user2_id) values ((select user_id from users where user_name = ?),(select user_id from users where user_name = ?));'
+            var inserts = [user1, user2];
+            sql = mysql.format(sql, inserts);
+
+            connPool.query(sql, function(err, results, fields) {
+                if(err) throw err;
+            });
+        }
+    })
+
+    var lastSQL = 'insert into messages (chat_id, message, timestamp, user) values ((select chat_id from chat where user1 = ? and user2 = ?),?, ?, ?)'
+    var lastInserts = [user1, user2, message, timestamp, user1];
+    lastSQL = mysql.format(lastSQL, lastInserts);
+
+    console.log(sql);
+
+        return new Promise((resolve, reject) =>
+            connPool.query(lastSQL, function(err, results, fields) {
+                if(err) throw err;
+                console.log("Result - ", results);
+                
+                resolve(results);
+            })
+        );
+
+}
 //Remove chats
 export async function deleteChat() { }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import express, {json} from "express";
+import express, {json, query} from "express";
 import dotenv from "dotenv";
+import {validateUser, validateUsers, addMessage} from "./database.js";
 dotenv.config({ path: "Properties.env"});
 
 const port = process.env.PORT;
@@ -10,9 +11,55 @@ server.use(express.json());
 // --------------------------------------------------------------
 
 //Get all messages
-server.get('/messages', function(req,res) {
+server.get('/userMessages', function(req,res) {
+    const user = req.query.user;
+    console.log(user);
 
+    validateUsers([user]).then((queryResults) => {
+        console.log(queryResults[0]);
+        if(queryResults[0]) {
+            console.log("User exists");
+
+        } else {
+            return res.status(200).json({
+                error: true,
+                message: "User " + user + " does not exist!"
+            });
+        }
+    });
 }) 
+
+server.post('/addMessages', function(req, res) {
+    const user1 = req.body.user1;
+    const user2 = req.body.user2;
+    const message = req.body.message;
+
+    let users = [user1, user2];
+    //Validate user
+    validateUsers(users).then((result) => {
+        console.log(result);
+        if(result == "Success") {
+            console.log("Users exists");
+            addMessage(user1, user2, message).then((result) => {
+                console.log(result);
+                if (result) {
+                    res.send("Success");
+                } else {
+                    res.status(500).json({
+                        error: true,
+                        message: "Error on last insert"
+                    });
+                }
+            })
+
+        } else {
+            return res.status(200).json({
+                error: true,
+                message: "Users do not exist!"
+            });
+        }
+    });
+})
 
 server.listen(port, () => {
     console.log("Express server listening on port ", port);
